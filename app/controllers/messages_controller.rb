32cac27faa582118c8e949a834d66506bf1d7a95
yoshinarinoda <attachmiso@gmail.com>
2016-02-17 13:16:22 +0000

    making age field

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -35,7 +35,7 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   ## ここまで
     def set_message
